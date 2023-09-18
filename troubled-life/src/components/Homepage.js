2e372e93a45a8f48b7415d14a71c65b35a31e112
DCaldwell13 <draykcaldwell0516@gmail.com>
2023-09-17 18:01:41 -0700

    ...

--- troubled-life/src/components/Homepage.js
+++ troubled-life/src/components/Homepage.js
@@ -11,12 +11,3 @@ function Homepage() {
 }
 
 export default Homepage;
-
-/* In this component structure, the `Homepage` component represents the homepage section of your website. It includes a headline, a description, and a call-to-action button.
-
-You can customize the content of the `h1`, `p`, and `a` tags to display your desired heading, description, and call-to-action text. Additionally, you can add CSS classes and styles to the component to achieve the desired visual appearance.
-
-Remember to import and use the `Homepage` component in your `App.js` file.
-
-Overview:
-The `Homepage` component is a functional component that represents the homepage section of the Troubled Life clothing web application. It displays a headline, description, and a call-to-action button. */
